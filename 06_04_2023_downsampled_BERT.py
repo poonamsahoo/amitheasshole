639479fb71d8548ac1e41e788d26dd2d56df2632
Poonam Sahoo <58399105+poonamsahoo@users.noreply.github.com>
2023-06-08 15:50:02 -0700

    Add files via upload

---
+++ 06_04_2023_downsampled_BERT.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import torch
+import numpy as np
+from transformers import BertTokenizer
+from torch import nn
+from transformers import BertModel
+from torch.optim import Adam
+from tqdm import tqdm
+
+
+# In[ ]:
+
+
+class BertClassifier(nn.Module):
+
+    def __init__(self, dropout=0.5):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, 2)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
+
+
+# In[2]:
+
+
+df = pd.read_csv("downsampled_dataset_156980.csv")
+
+
+# In[7]:
+
+
+df['self_text'] = df['self_text'].str.lower()
+
+
+# In[9]:
+
+
+df['link_flair_text'] = df['link_flair_text'].str.lower()
+
+
+# In[11]:
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+labels = {'asshole':0,
+          'not the a-hole':1,
+          }
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, df):
+
+        self.labels = [labels[label] for label in df['link_flair_text']]
+        self.texts = [tokenizer(text, 
+                               padding='max_length', max_length = 512, truncation=True,
+                                return_tensors="pt") for text in df['self_text']]
+
+    def classes(self):
+        return self.labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_batch_labels(self, idx):
+        # Fetch a batch of labels
+        return np.array(self.labels[idx])
+
+    def get_batch_texts(self, idx):
+        # Fetch a batch of inputs
+        return self.texts[idx]
+
+    def __getitem__(self, idx):
+
+        batch_texts = self.get_batch_texts(idx)
+        batch_y = self.get_batch_labels(idx)
+
+        return batch_texts, batch_y
+
+
+# In[12]:
+
+
+np.random.seed(12)
+df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42), 
+                                     [int(.8*len(df)), int(.9*len(df))])
+
+print(len(df_train),len(df_val), len(df_test))
+
+
+# In[14]:
+
+
+def train(model, train_data, val_data, learning_rate, epochs):
+
+    train, val = Dataset(train_data), Dataset(val_data)
+
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr= learning_rate)
+
+    if use_cuda:
+
+            model = model.cuda()
+            criterion = criterion.cuda()
+
+    for epoch_num in range(epochs):
+
+            total_acc_train = 0
+            total_loss_train = 0
+
+            for train_input, train_label in tqdm(train_dataloader):
+
+                train_label = train_label.to(device)
+                mask = train_input['attention_mask'].to(device)
+                input_id = train_input['input_ids'].squeeze(1).to(device)
+
+                output = model(input_id, mask)
+                
+                batch_loss = criterion(output, train_label.long())
+                total_loss_train += batch_loss.item()
+                
+                acc = (output.argmax(dim=1) == train_label).sum().item()
+                total_acc_train += acc
+
+                model.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+            
+            total_acc_val = 0
+            total_loss_val = 0
+
+            with torch.no_grad():
+
+                for val_input, val_label in val_dataloader:
+
+                    val_label = val_label.to(device)
+                    mask = val_input['attention_mask'].to(device)
+                    input_id = val_input['input_ids'].squeeze(1).to(device)
+
+                    output = model(input_id, mask)
+
+                    batch_loss = criterion(output, val_label.long())
+                    total_loss_val += batch_loss.item()
+                    
+                    acc = (output.argmax(dim=1) == val_label).sum().item()
+                    total_acc_val += acc
+            
+            print(
+                f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+                | Train Accuracy: {total_acc_train / len(train_data): .3f} \
+                | Val Loss: {total_loss_val / len(val_data): .3f} \
+                | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+                  
+EPOCHS = 5
+model = BertClassifier()
+LR = 1e-6
+              
+train(model, df_train, df_val, LR, EPOCHS)
+
